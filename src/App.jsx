ce9720e0306fce81c7fd8e5dc36219330826797c
Steven Ho <malloryvfonte@gmail.com>
2025-01-01 21:02:32 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import WebDevMain from "./pages/WebDevMain";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
-  console.log("test");
   return (
     <BrowserRouter basename="/Portfolio-2.0">
-      {" "}
       {/* Replace with your repo name */}
       <Routes>
         <Route path="/" element={<WebDevMain />} />
